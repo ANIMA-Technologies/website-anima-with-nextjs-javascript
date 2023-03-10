baa50677655a4fdb6d2a33c16e0ca424547de41e
Bosco <heriniainajeanboscof@gmail.com>
2023-03-10 16:26:16 +0300

    feature: add navigation link to all developpement related pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 ;import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
 import style from "../styles/StyleForAll.module.css";
 const Navbar = () => {
@@ -6,16 +7,16 @@ const Navbar = () => {
     <div className={`"container-fluid" ${style.containerFluid}`}>
       <nav className="navbar navbar-expand-lg navbar-light">
  
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" href="/">
             <Image src="/imagesAnima/Logo.png" className={styles.logo} alt="logo" width={50} height={50} />
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className={`nav-link  ${styles.navLink}  active`} aria-current="page" href="#">Home</a>
+                <Link className={`nav-link  ${styles.navLink}  active`} aria-current="page" href="/">Accueil</Link>
               </li>
               
               <li className="nav-item dropdown">
@@ -24,10 +25,10 @@ const Navbar = () => {
                   <i className="fa fa-angle-down ms-1 font-weight-bold" aria-hidden="true"></i>
                 </a>
                 <ul className={`dropdown-menu ${styles.dropdown1}`} aria-labelledby="navbarDropdown">
-                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Développement web</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Développement MOBILE</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`}>Développement de logiciels sur mesure</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Design de marque</a></li>
+                  <li><Link className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="/developpement/web">Développement web</Link></li>
+                  <li><Link className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="/developpement/mobile">Développement MOBILE</Link></li>
+                  <li><Link className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="/developpement/logiciels-sur-mesure">Développement de logiciels sur mesure</Link></li>
+                  <li><Link className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="/developpement/design-de-marque">Design de marque</Link></li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
