b3d3a25e1d5c15c5dd4ce628969c3b4d8f58561d
faniry <faniryrakotofamola@gmail.com>
2023-03-10 15:54:48 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light">
  
           <a className="navbar-brand" href="#">
-            <Image src="/imagesAnima/Logo.png" classNameName={styles.logo} alt="logo" width={50} height={50} />
+            <Image src="/imagesAnima/Logo.png" className={styles.logo} alt="logo" width={50} height={50} />
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -19,26 +19,26 @@ const Navbar = () => {
               </li>
               
               <li className="nav-item dropdown">
-                <a className={`nav-link dropdown-toggle  ${styles.navLink}  active`} href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a className={`nav-link dropdown-toggle  ${styles.dropdownToggle}  ${styles.navLink}  active`} href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Développement Informatique
-                  <i className="fa fa-angle-down ml-1 font-weight-bold" aria-hidden="true"></i>
+                  <i className="fa fa-angle-down ms-1 font-weight-bold" aria-hidden="true"></i>
                 </a>
                 <ul className={`dropdown-menu ${styles.dropdown1}`} aria-labelledby="navbarDropdown">
-                  <li><a className={`dropdown-item ${styles.dropdownItem0}`} href="#">Développement web</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem0}`} href="#">Développement MOBILE</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem0}`}>Développement de logiciels sur mesure</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem0}`} href="#">Design de marque</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Développement web</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Développement MOBILE</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`}>Développement de logiciels sur mesure</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem0}`} href="#">Design de marque</a></li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
-                <a className={`nav-link dropdown-toggle  ${styles.navLink}`} href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a className={`nav-link dropdown-toggle ${styles.dropdownToggle}  ${styles.navLink}`} href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   La société
-                  <i className="fa fa-angle-down ml-1 font-weight-bold" aria-hidden="true"></i>
+                  <i className="fa fa-angle-down ms-1 font-weight-bold" aria-hidden="true"></i>
                 </a>
                 <ul className={`dropdown-menu ${styles.dropdown2}`} aria-labelledby="navbarDropdown">
-                  <li><a className={`dropdown-item ${styles.dropdownItem1}`} href="#">Réalisations</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem1}`} href="#">L’équipe</a></li>
-                  <li><a className={`dropdown-item ${styles.dropdownItem1}`} href="#">Recrutement</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem1}`} href="#">Réalisations</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem1}`} href="#">L’équipe</a></li>
+                  <li><a className={`dropdown-item ${styles.dropdownItem} ${styles.dropdownItem1}`} href="#">Recrutement</a></li>
                 </ul>
               </li>
               <li className="nav-item">
